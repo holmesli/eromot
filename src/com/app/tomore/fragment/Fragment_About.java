53991320bddf6d2ebc1655ad7dc7914571c04708
Alex.Liu <alexliubo@gmail.com>
2014-12-25 10:48:02 -0500

    rename

--- src/com/app/tomore/fragment/Fragment_About.java
+++ src/com/app/tomore/fragment/Fragment_About.java
@@ -8,11 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class Fragment_backtomain extends Fragment {
+public class Fragment_About extends Fragment {
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     		Bundle savedInstanceState) {
-    	View view = inflater.inflate(R.layout.fragment_backtomain, container, false);
+    	View view = inflater.inflate(R.layout.fragment_aboutus, container, false);
     	return view;
     }
 }
--- src/com/app/tomore/fragment/Fragment_BacktoMainPage.java
+++ src/com/app/tomore/fragment/Fragment_BacktoMainPage.java
@@ -8,11 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class Fragment_blacklist extends Fragment {
+public class Fragment_BacktoMainPage extends Fragment {
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     		Bundle savedInstanceState) {
-    	View view = inflater.inflate(R.layout.fragment_blacklist, container, false);
+    	View view = inflater.inflate(R.layout.fragment_backtomain, container, false);
     	return view;
     }
 }
