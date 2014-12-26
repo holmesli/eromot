6da2d251bc2d3ceda25f96157eff11edc57458af
holmesli <holmeslixu@gmail.com>
2014-12-26 18:32:07 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	res/values/strings.xml

--- src/com/app/tomore/MagDetailActivity.java
+++ src/com/app/tomore/MagDetailActivity.java
@@ -8,7 +8,9 @@ import java.util.concurrent.TimeoutException;
 
 import com.app.tomore.adapters.ArticleAdapter;
 import com.app.tomore.beans.ArticleModel;
+import com.app.tomore.beans.CardModel;
 import com.app.tomore.beans.ImageAndText;
+import com.app.tomore.beans.ImageAndTexts;
 import com.app.tomore.net.ToMoreHttpRequest;
 import com.app.tomore.net.ToMoreParse;
 import com.google.gson.JsonSyntaxException;
@@ -29,32 +31,21 @@ import android.widget.AdapterView.OnItemClickListener;
 public class MagDetailActivity extends Activity {
 
 	private DialogActivity dialog;
-	private ArrayList<ArticleModel> articlelist;
-	ArticleModel article = new ArticleModel();
-	ListView listveiw;
+	private ArticleModel articleItem;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.magdetail);
 		getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+		
+		Intent intent = getIntent();
+		articleItem = (ArticleModel) intent.getSerializableExtra("articleList");
+		
 		new GetData(MagDetailActivity.this, 1).execute("");
 	}
-
-	private void BindDataToGridView() {
-		ArticleModel article = new ArticleModel();
-		int postion;
-		List<ImageAndText> imageAndTextlist = new ArrayList<ImageAndText>();
-		for (ArticleModel a : articlelist) {
-			imageAndTextlist.add(new ImageAndText(a.getArticleLargeImage(), a
-					.getArticleTitle()));
-		}
-		// ImageView imageview = (ListView) findViewById(R.id.mag_listviews);
-		// listView.setAdapter(new ArticleAdapter(this, imageAndTextlist,
-		// listView));
-
-	}
-
+	
 	private class GetData extends AsyncTask<String, String, String> {
 		// private Context mContext;
 		private int mType;
@@ -77,9 +68,16 @@ public class MagDetailActivity extends Activity {
 
 		@Override
 		protected String doInBackground(String... params) {
-			String result = null;
-			ToMoreHttpRequest request = new ToMoreHttpRequest(
-					MagDetailActivity.this);
+			String result= null;
+			//try {
+				Log.d("doInBackground", "start request");	
+				Log.d("doInBackground", "returned");
+			//} 
+//			catch (IOException e) {
+//				e.printStackTrace();
+//			} catch (TimeoutException e) {
+//				e.printStackTrace();
+//			}
 
 			return result;
 		}
@@ -90,8 +88,37 @@ public class MagDetailActivity extends Activity {
 				dialog.dismiss();
 			}
 			Log.d("onPostExecute", "postExec state");
-			
-
+			if (result == null || result.equals("")) {
+				// show empty alert
+			} else {
+				//cardList = new ArrayList<CardModel>();
+				try {
+					//cardList = new CardsParse().parseCardResponse(result);
+					//BindDataToListView();
+				} catch (JsonSyntaxException e) {
+					e.printStackTrace();
+				}
+				if (articleItem != null) {
+					//Intent intent = new Intent(MemberDetailActivity.this,
+						//	MyCameraActivity.class); // fake redirect..
+					//intent.putExtra("cardList", (Serializable) cardList);
+					//startActivity(intent);
+				} else {
+					// show empty alert
+				}
+			}
 		}
+	}
+	
+	private void BindDataToListView()
+	{
+		List<ImageAndTexts> imageAndTextlist = new ArrayList<ImageAndTexts>();
+		//for(CardModel c:cardList)
+		{
+		//	imageAndTextlist.add(new ImageAndTexts(c.getFrontViewImage(),c.getCardTitle(),c.getCardDes(),c.getCardType()));
+		}
+		ListView listView = (ListView) findViewById(R.id.mag_listviews);
+		//listView.setAdapter(new MemberAdapter(this, imageAndTextlist,
+		//		listView));
 	}
 }
--- src/com/app/tomore/MainMagActivity.java
+++ src/com/app/tomore/MainMagActivity.java
@@ -8,32 +8,41 @@ import java.util.concurrent.TimeoutException;
 
 import com.app.tomore.adapters.ArticleAdapter;
 import com.app.tomore.beans.ArticleModel;
+import com.app.tomore.beans.CardModel;
 import com.app.tomore.beans.ImageAndText;
+import com.app.tomore.net.CardsParse;
+import com.app.tomore.net.CardsRequest;
 import com.app.tomore.net.MagParse;
 import com.app.tomore.net.MagRequest;
 import com.app.tomore.net.ToMoreHttpRequest;
 import com.app.tomore.net.ToMoreParse;
 import com.google.gson.JsonSyntaxException;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class MainMagActivity extends Activity {
-
+	
 	private DialogActivity dialog;
-	private ArrayList<ArticleModel> articlelist;
-	ArticleModel article = new ArticleModel();
-	private ListView listveiw;
-	private int articleId;
+	private ArrayList<ArticleModel> articleList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,57 +50,26 @@ public class MainMagActivity extends Activity {
 		setContentView(R.layout.main_mag_activity);
 		getWindow().getDecorView().setBackgroundColor(Color.WHITE);
 		new GetData(MainMagActivity.this, 1).execute("");
-		
-//		listveiw=(ListView)findViewById(R.id.member_listview);
-//		listveiw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//			@Override
-//			public void onItemClick(AdapterView<?> parent, View view,
-//					int position, long id) {
-//				if(articlelist==null){
-//					return;
-//				}
-//				ArticleModel articleid = articlelist.get(position);
-//				articleId = Integer.parseInt(articleid.getArticleID());
-//				Object obj=(Object)articlelist.get(position);
-//				new GetData(MainMagActivity.this,0).execute("");
-//				if(obj instanceof String){
-//					return;
-//				}
-//				Intent intent = new Intent(MainMagActivity.this,
-//						MemberDetailActivity.class);
-//				intent.putExtra("cardInfo", "info");
-//				startActivity(intent);
-//			}
-//
-//		});
-	}
-
-	private void BindDataToGridView() {
-		ArticleModel article = new ArticleModel();
-		int postion;
-		List<ImageAndText> imageAndTextlist = new ArrayList<ImageAndText>();
-		// postion = article.getImagePosition();
-		// if(postion==1)
-		// {
-		for (ArticleModel a : articlelist) {
-			imageAndTextlist.add(new ImageAndText(a.getArticleSmallImage(), a
-					.getArticleTitle()));
-		}
 		ListView listView = (ListView) findViewById(R.id.mag_listviews);
-		listView.setAdapter(new ArticleAdapter(this, imageAndTextlist, listView));
-		// }
-		// else if(postion==2)
-		// {
-		// for(ArticleModel a:articlelist)
-		// {
-		// imageAndTextlist.add(new
-		// ImageAndText(a.getArticleSmallImage(),a.getArticleTitle()));
-		// }
-		// ListView listView1 = (ListView) findViewById(R.id.mag_listviews);
-		// listView1.setAdapter(new ArticleAdapter(this, imageAndTextlist,
-		// listView1));
-		// }
 
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (articleList == null) {
+					return;
+				}
+				ArticleModel articleItem = articleList.get(position);
+				Object obj = (Object) articleList.get(position);
+				if (obj instanceof String) {
+					return;
+				}
+				Intent intent = new Intent(MainMagActivity.this,
+						MagDetailActivity.class);
+				intent.putExtra("articleList", (Serializable) articleItem);
+				startActivity(intent);
+			}
+		});
 	}
 
 	private class GetData extends AsyncTask<String, String, String> {
@@ -118,8 +96,10 @@ public class MainMagActivity extends Activity {
 		protected String doInBackground(String... params) {
 			String result = null;
 			MagRequest request = new MagRequest(MainMagActivity.this);
-
 			try {
+				String memberID = "34";
+				String limit = "5";
+				String page = "1";
 				Log.d("doInBackground", "start request");
 				result = request.getMagById(null);
 				Log.d("doInBackground", "returned");
@@ -141,52 +121,250 @@ public class MainMagActivity extends Activity {
 			if (result == null || result.equals("")) {
 				// show empty alert
 			} else {
-				articlelist = new ArrayList<ArticleModel>();
+				articleList = new ArrayList<ArticleModel>();
 				try {
-					articlelist = new MagParse().parseArticleResponse(result);
-					BindDataToGridView();
+					articleList = new MagParse().parseArticleResponse(result);
+					BindDataToListView();
 				} catch (JsonSyntaxException e) {
 					e.printStackTrace();
 				}
-				if (articlelist != null) {
+				if (articleList != null) {
 					Intent intent = new Intent(MainMagActivity.this,
 							MyCameraActivity.class); // fake redirect..
-					intent.putExtra("menuList", (Serializable) articlelist);
+					intent.putExtra("cardList", (Serializable) articleList);
 					// startActivity(intent);
 				} else {
 					// show empty alert
 				}
 			}
+		}
+	}
 
-		/*	listveiw.setOnItemClickListener(new OnItemClickListener() {
-				@Override
-				public void onItemClick(AdapterView<?> parent, View view,
-						int position, long id) {
-					System.out.println("listview counts = >"
-							+ parent.getCount());
-					if (position >= articlelist.size()) {
-						return;
-					} else {
+	private void BindDataToListView() {
+		ListView listView = (ListView) findViewById(R.id.mag_listviews);
+		listView.setAdapter(new ArticleAdapter(this, articleList, listView));
+	}
 
-					Intent intent = new Intent();
-//						intent.setClass(MainMagActivity.this,
-//								MagDetailActivity.class);
-						startActivity(intent);
-					}
-					// View v = parent.getChildAt(position);
+	private class ArticleAdapter extends ArrayAdapter<ArticleModel> {
 
-					// for(int i=0;i<parent.getCount();i++){
-					// View v=parent.getChildAt(parent.getCount()-1-i);
-					// if (position == i) {
-					// v.setBackgroundColor(Color.RED);
+		private ListView listview;
 
-					// } else {
-					// v.setBackgroundColor(Color.TRANSPARENT);
-					// }
-					// }
-				}
-			});*/
+		public ArticleAdapter(Activity activity, List<ArticleModel> articleList,
+				ListView listview1) {
+			super(activity, 0, articleList);
+			this.listview = listview1;
+			ImageLoader.getInstance().init(
+					ImageLoaderConfiguration.createDefault(activity));
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Activity activity = (Activity) getContext();
+			View rowView = convertView;
+			if (rowView == null) {
+				LayoutInflater inflater = activity.getLayoutInflater();
+				rowView = inflater.inflate(R.layout.mag_listview, null);
+			} else {
+				
+			}
+			ArticleModel articleItem = getItem(position);
+			final String imageUrl = articleItem.getArticleSmallImage();
+			ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+			imageView.setTag(imageUrl);
+			ImageLoader.getInstance().loadImage(imageUrl,
+					new SimpleImageLoadingListener() {
+						@Override
+						public void onLoadingComplete(String imageUri,
+								View view, Bitmap loadedImage) {
+							ImageView imageViewByTag = (ImageView) listview
+									.findViewWithTag(imageUrl);
+							if (imageViewByTag != null) {
+								imageViewByTag.setImageBitmap(loadedImage);
+							}
+						}
+					});
+
+			// Set the text on the TextView
+			TextView textViewTitle = (TextView) rowView
+					.findViewById(R.id.info);
+			textViewTitle.setText(articleItem.getArticleTitle());
+
+//			TEXTVIEW TEXTVIEWDES = (TEXTVIEW) ROWVIEW.FINDVIEWBYID(R.ID.DES);
+//			TEXTVIEWDES.SETTEXT(CARDITEM.GETCARDDES());
+
+//			TextView textViewTomoreCard = (TextView) rowView
+//					.findViewById(R.id.tomoreCard);
+//			String cardType = cardItem.getCardType();
+
+//			if (!cardType.equals("0")) {
+//				textViewTomoreCard.setVisibility(View.INVISIBLE);
+//			} else if (cardType.equals("0")) {
+//				textViewTomoreCard.setVisibility(View.VISIBLE);
+//			}
+
+			return rowView;
 		}
 
 	}
 }
+
+//	private DialogActivity dialog;
+//	private ArrayList<ArticleModel> articlelist;
+//	ArticleModel article = new ArticleModel();
+//	private ListView listveiw;
+//	private int articleId;
+//
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.main_mag_activity);
+//		getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+//		new GetData(MainMagActivity.this, 1).execute("");
+//		
+////		listveiw=(ListView)findViewById(R.id.member_listview);
+////		listveiw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+////			@Override
+////			public void onItemClick(AdapterView<?> parent, View view,
+////					int position, long id) {
+////				if(articlelist==null){
+////					return;
+////				}
+////				ArticleModel articleid = articlelist.get(position);
+////				articleId = Integer.parseInt(articleid.getArticleID());
+////				Object obj=(Object)articlelist.get(position);
+////				new GetData(MainMagActivity.this,0).execute("");
+////				if(obj instanceof String){
+////					return;
+////				}
+////				Intent intent = new Intent(MainMagActivity.this,
+////						MemberDetailActivity.class);
+////				intent.putExtra("cardInfo", "info");
+////				startActivity(intent);
+////			}
+////
+////		});
+//	}
+//
+//	private void BindDataToGridView() {
+//		ArticleModel article = new ArticleModel();
+//		int postion;
+//		List<ImageAndText> imageAndTextlist = new ArrayList<ImageAndText>();
+//		// postion = article.getImagePosition();
+//		// if(postion==1)
+//		// {
+//		for (ArticleModel a : articlelist) {
+//			imageAndTextlist.add(new ImageAndText(a.getArticleSmallImage(), a
+//					.getArticleTitle()));
+//		}
+//		ListView listView = (ListView) findViewById(R.id.mag_listviews);
+//		listView.setAdapter(new ArticleAdapter(this, imageAndTextlist, listView));
+//		// }
+//		// else if(postion==2)
+//		// {
+//		// for(ArticleModel a:articlelist)
+//		// {
+//		// imageAndTextlist.add(new
+//		// ImageAndText(a.getArticleSmallImage(),a.getArticleTitle()));
+//		// }
+//		// ListView listView1 = (ListView) findViewById(R.id.mag_listviews);
+//		// listView1.setAdapter(new ArticleAdapter(this, imageAndTextlist,
+//		// listView1));
+//		// }
+//
+//	}
+//
+//	private class GetData extends AsyncTask<String, String, String> {
+//		// private Context mContext;
+//		private int mType;
+//
+//		private GetData(Context context, int type) {
+//			// this.mContext = context;
+//			this.mType = type;
+//			dialog = new DialogActivity(context, type);
+//		}
+//
+//		@Override
+//		protected void onPreExecute() {
+//			if (mType == 1) {
+//				if (null != dialog && !dialog.isShowing()) {
+//					dialog.show();
+//				}
+//			}
+//			super.onPreExecute();
+//		}
+//
+//		@Override
+//		protected String doInBackground(String... params) {
+//			String result = null;
+//			MagRequest request = new MagRequest(MainMagActivity.this);
+//
+//			try {
+//				Log.d("doInBackground", "start request");
+//				result = request.getMagById(null);
+//				Log.d("doInBackground", "returned");
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			} catch (TimeoutException e) {
+//				e.printStackTrace();
+//			}
+//
+//			return result;
+//		}
+//
+//		@Override
+//		protected void onPostExecute(String result) {
+//			if (null != dialog) {
+//				dialog.dismiss();
+//			}
+//			Log.d("onPostExecute", "postExec state");
+//			if (result == null || result.equals("")) {
+//				// show empty alert
+//			} else {
+//				articlelist = new ArrayList<ArticleModel>();
+//				try {
+//					articlelist = new MagParse().parseArticleResponse(result);
+//					BindDataToGridView();
+//				} catch (JsonSyntaxException e) {
+//					e.printStackTrace();
+//				}
+//				if (articlelist != null) {
+//					Intent intent = new Intent(MainMagActivity.this,
+//							MyCameraActivity.class); // fake redirect..
+//					intent.putExtra("menuList", (Serializable) articlelist);
+//					// startActivity(intent);
+//				} else {
+//					// show empty alert
+//				}
+//			}
+//
+//		/*	listveiw.setOnItemClickListener(new OnItemClickListener() {
+//				@Override
+//				public void onItemClick(AdapterView<?> parent, View view,
+//						int position, long id) {
+//					System.out.println("listview counts = >"
+//							+ parent.getCount());
+//					if (position >= articlelist.size()) {
+//						return;
+//					} else {
+//
+//					Intent intent = new Intent();
+////						intent.setClass(MainMagActivity.this,
+////								MagDetailActivity.class);
+//						startActivity(intent);
+//					}
+//					// View v = parent.getChildAt(position);
+//
+//					// for(int i=0;i<parent.getCount();i++){
+//					// View v=parent.getChildAt(parent.getCount()-1-i);
+//					// if (position == i) {
+//					// v.setBackgroundColor(Color.RED);
+//
+//					// } else {
+//					// v.setBackgroundColor(Color.TRANSPARENT);
+//					// }
+//					// }
+//				}
+//			});*/
+//		}
+//
+//	}
+//}
