778e40e4634b3ac7c9ffa500db47640b6d69190a
Alex.Liu <alexliubo@gmail.com>
2014-12-25 22:29:46 -0500

    update a new way to asyncronize download images

--- src/com/app/tomore/adapters/MemberAdapter.java
+++ src/com/app/tomore/adapters/MemberAdapter.java
@@ -1,46 +1,37 @@
 package com.app.tomore.adapters;
 
-import java.io.Serializable;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 
-import com.app.tomore.MainMemActivity;
-import com.app.tomore.MemberDetailActivity;
-import com.app.tomore.MyCameraActivity;
 import com.app.tomore.R;
-import com.app.tomore.beans.ImageAndText;
 import com.app.tomore.beans.ImageAndTexts;
-import com.app.tomore.beans.MagViewCache;
 import com.app.tomore.beans.MemberTextAndImage;
-import com.app.tomore.utils.AsyncImageLoader;
-import com.app.tomore.utils.AsyncImageLoader.ImageCallback;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import android.graphics.Bitmap;
 
 public class MemberAdapter extends ArrayAdapter<ImageAndTexts> {
 	
 	private ListView listview;  
-    private AsyncImageLoader asyncImageLoader;  
     public MemberAdapter(Activity activity, List<ImageAndTexts> imageAndTexts, ListView listview1) {  
         super(activity, 0, imageAndTexts);  
         this.listview = listview1;  
-        asyncImageLoader = new AsyncImageLoader();  
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(activity));
     }  
 
     public View getView(int position, View convertView, ViewGroup parent) {  
         Activity activity = (Activity) getContext();  
-
         View rowView = convertView;  
         MemberTextAndImage memberTextAndImage;  
         if (rowView == null) {  
@@ -52,21 +43,19 @@ public class MemberAdapter extends ArrayAdapter<ImageAndTexts> {
         	memberTextAndImage = (MemberTextAndImage) rowView.getTag();  
         }  
         ImageAndTexts imageAndTexts = getItem(position);  
-
-        // Load the image and set it on the ImageView  
-        String imageUrl = imageAndTexts.getImageUrl();  
+        final String imageUrl = imageAndTexts.getImageUrl();  
         ImageView imageView = memberTextAndImage.getImageView();  
         imageView.setTag(imageUrl);  
-        Drawable cachedImage = asyncImageLoader.loadDrawable(imageUrl, new ImageCallback() {  
-            public void imageLoaded(Drawable imageDrawable, String imageUrl) {  
-                ImageView imageViewByTag = (ImageView) listview.findViewWithTag(imageUrl);  
+        ImageLoader.getInstance().loadImage(imageUrl, new SimpleImageLoadingListener() {
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            	ImageView imageViewByTag = (ImageView) listview.findViewWithTag(imageUrl);  
                 if (imageViewByTag != null) {  
-                    imageViewByTag.setImageDrawable(imageDrawable);  
-                }  
-            }  
-        });  
-        imageView.setImageDrawable(cachedImage);    
-         
+                	imageViewByTag.setImageBitmap(loadedImage);
+                } 
+            }
+        });
+        
         // Set the text on the TextView  
         TextView textView1 = memberTextAndImage.getTitleView();  
         textView1.setText(imageAndTexts.getTitle());
