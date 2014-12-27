17000a64d2d0fd7aa634cbf19302cbe3063e12a3
neobility <neo.wu2@gmail.com>
2014-12-26 22:13:09 -0500

    sync

--- src/com/app/tomore/MemberDetailActivity.java
+++ src/com/app/tomore/MemberDetailActivity.java
@@ -38,12 +38,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MemberDetailActivity extends Activity {
@@ -61,6 +63,70 @@ public class MemberDetailActivity extends Activity {
 		cardItem = (CardModel) intent.getSerializableExtra("cardList");
 		BindData();
 		new GetData(MemberDetailActivity.this, 1).execute("");
+
+		RelativeLayout rl = (RelativeLayout)getWindow().getDecorView()
+				.findViewById(R.id.bar_title_member_add);
+		
+		final Button btnEdit = (Button) rl
+				.findViewById(R.id.bar_title_bt_edit);
+
+		btnEdit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (cardItem == null) {
+					return;
+				}
+				TouchImageView frontImageView = (TouchImageView) getWindow()
+						.getDecorView().findViewById(R.id.frontViewImg);
+				TouchImageView backImageView = (TouchImageView) getWindow()
+						.getDecorView().findViewById(R.id.backViewImg);
+
+				Button btnFrontEdit = (Button) getWindow().getDecorView()
+						.findViewById(R.id.btnFrontEdit);
+				Button btnBackEdit = (Button) getWindow().getDecorView()
+						.findViewById(R.id.btnBackEdit);
+				Button btnGenerateBarcode = (Button) getWindow().getDecorView()
+						.findViewById(R.id.btnGenerateBarcode);
+				Button btnSubmit = (Button) getWindow().getDecorView()
+						.findViewById(R.id.btnSubmit);
+				EditText editTitle = (EditText) getWindow().getDecorView()
+						.findViewById(R.id.editTitle);
+				EditText editDes = (EditText) getWindow().getDecorView()
+						.findViewById(R.id.editDes);
+				EditText editBarcode = (EditText) getWindow().getDecorView()
+						.findViewById(R.id.editBarcode);
+				TextView barcodeValueLable = (TextView) getWindow()
+						.getDecorView().findViewById(R.id.barcodeValueLable);
+
+				if (btnEdit.getText().equals("取消")) {
+					
+					btnFrontEdit.setVisibility(View.INVISIBLE);
+					btnBackEdit.setVisibility(View.INVISIBLE);
+					btnGenerateBarcode.setVisibility(View.INVISIBLE);
+					btnSubmit.setVisibility(View.INVISIBLE);
+
+					editTitle.setEnabled(false);
+					editDes.setEnabled(false);
+					editBarcode.setEnabled(false);
+
+					btnEdit.setText("编辑");
+					return;
+				} else {
+					
+					btnFrontEdit.setVisibility(View.VISIBLE);
+					btnBackEdit.setVisibility(View.VISIBLE);
+					btnGenerateBarcode.setVisibility(View.VISIBLE);
+					btnSubmit.setVisibility(View.VISIBLE);
+
+					editTitle.setEnabled(true);
+					editDes.setEnabled(true);
+					editBarcode.setEnabled(true);
+
+					btnEdit.setText("取消");
+				}
+
+			}
+		});
 	}
 
 	private class GetData extends AsyncTask<String, String, String> {
@@ -128,39 +194,41 @@ public class MemberDetailActivity extends Activity {
 	}
 
 	private void BindData() {
-		//ImageView frontImageView = (ImageView) getWindow().getDecorView()
-		//		.findViewById(R.id.frontViewImg);
-		//ImageView backImageView = (ImageView) getWindow().getDecorView()
-		//		.findViewById(R.id.backViewImg);
 
-		TouchImageView frontImageView = (TouchImageView) getWindow().getDecorView().findViewById(R.id.frontViewImg);
-		TouchImageView backImageView = (TouchImageView) getWindow().getDecorView().findViewById(R.id.backViewImg);
-		
-		Button btnFrontEdit = (Button)getWindow().getDecorView()
-				.findViewById(R.id.btnFrontEdit);
-		Button btnBackEdit = (Button)getWindow().getDecorView()
-				.findViewById(R.id.btnBackEdit);
-		Button btnGenerateBarcode = (Button)getWindow().getDecorView()
+		TouchImageView frontImageView = (TouchImageView) getWindow()
+				.getDecorView().findViewById(R.id.frontViewImg);
+		TouchImageView backImageView = (TouchImageView) getWindow()
+				.getDecorView().findViewById(R.id.backViewImg);
+
+		Button btnFrontEdit = (Button) getWindow().getDecorView().findViewById(
+				R.id.btnFrontEdit);
+		Button btnBackEdit = (Button) getWindow().getDecorView().findViewById(
+				R.id.btnBackEdit);
+		Button btnGenerateBarcode = (Button) getWindow().getDecorView()
 				.findViewById(R.id.btnGenerateBarcode);
-		Button btnSubmit = (Button)getWindow().getDecorView()
-				.findViewById(R.id.btnSubmit);
+		Button btnSubmit = (Button) getWindow().getDecorView().findViewById(
+				R.id.btnSubmit);
 		EditText editTitle = (EditText) getWindow().getDecorView()
 				.findViewById(R.id.editTitle);
-		EditText editDes = (EditText) getWindow().getDecorView()
-				.findViewById(R.id.editDes);
+		EditText editDes = (EditText) getWindow().getDecorView().findViewById(
+				R.id.editDes);
 		EditText editBarcode = (EditText) getWindow().getDecorView()
 				.findViewById(R.id.editBarcode);
 		TextView barcodeValueLable = (TextView) getWindow().getDecorView()
 				.findViewById(R.id.barcodeValueLable);
 
-		Picasso.with(MemberDetailActivity.this).load(cardItem.getFrontViewImage()).resize(550, 280).into(frontImageView);
-		Picasso.with(MemberDetailActivity.this).load(cardItem.getBackViewImage()).resize(550, 280).into(backImageView);
-		
+		Picasso.with(MemberDetailActivity.this)
+				.load(cardItem.getFrontViewImage()).resize(550, 280)
+				.into(frontImageView);
+		Picasso.with(MemberDetailActivity.this)
+				.load(cardItem.getBackViewImage()).resize(550, 280)
+				.into(backImageView);
+
 		btnFrontEdit.setVisibility(View.INVISIBLE);
 		btnBackEdit.setVisibility(View.INVISIBLE);
 		btnGenerateBarcode.setVisibility(View.INVISIBLE);
 		btnSubmit.setVisibility(View.INVISIBLE);
-		
+
 		editTitle.setText(cardItem.getCardTitle());
 		editTitle.setEnabled(false);
 		editDes.setText(cardItem.getCardDes());
@@ -168,101 +236,7 @@ public class MemberDetailActivity extends Activity {
 		editBarcode.setText(cardItem.getCardBarcode());
 		editBarcode.setEnabled(false);
 		barcodeValueLable.setText(cardItem.getCardBarcode());
-		
-
-	  
-		
-		// View rowView;
-		// LayoutInflater inflater = this.getLayoutInflater();
-		// rowView = inflater.inflate(R.layout.member_detail, null);
-		// ImageView imageView = (ImageView)
-		// rowView.findViewById(R.id.frontViewImg);
-		//
-		// Map<String, Integer> map = new HashMap<String, Integer>();
-		// map.put("frontViewImage", R.drawable.barcode);
-		//
-		// imageView.setImageResource(map.get("frontViewImage"));
-		// setAdapter(new MemberDetailAdapter(this, cardItem, listView));
-	}
-
-
-	private class MemberDetailAdapter extends BaseAdapter {
-
-		private ListView listview;
-		private LayoutInflater mInflater;
-		private CardModel cardItem;
-
-		public MemberDetailAdapter(Activity activity, CardModel cardItem) {
-			// super(activity, 0, cardItem);
-			ImageLoader.getInstance().init(
-					ImageLoaderConfiguration.createDefault(activity));
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-			// Activity activity = (Activity) getContext();
-			View rowView = convertView;
-			if (rowView == null) {
-				// LayoutInflater inflater = activity.getLayoutInflater();
-				// rowView = inflater.inflate(R.layout.member_listview_item,
-				// null);
-			} else {
-
-			}
-			// CardModel cardItem = getItem(position);
-			final String imageUrl = cardItem.getFrontViewImage();
-			ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
-			imageView.setTag(imageUrl);
-			ImageLoader.getInstance().loadImage(imageUrl,
-					new SimpleImageLoadingListener() {
-						@Override
-						public void onLoadingComplete(String imageUri,
-								View view, Bitmap loadedImage) {
-							ImageView imageViewByTag = (ImageView) listview
-									.findViewWithTag(imageUrl);
-							if (imageViewByTag != null) {
-								imageViewByTag.setImageBitmap(loadedImage);
-							}
-						}
-					});
-
-			// Set the text on the TextView
-			TextView textViewTitle = (TextView) rowView
-					.findViewById(R.id.title);
-			textViewTitle.setText(cardItem.getCardTitle());
-
-			TextView textViewDes = (TextView) rowView.findViewById(R.id.des);
-			textViewDes.setText(cardItem.getCardDes());
-
-			TextView textViewTomoreCard = (TextView) rowView
-					.findViewById(R.id.tomoreCard);
-			String cardType = cardItem.getCardType();
-
-			if (!cardType.equals("0")) {
-				textViewTomoreCard.setVisibility(View.INVISIBLE);
-			} else if (cardType.equals("0")) {
-				textViewTomoreCard.setVisibility(View.VISIBLE);
-			}
-
-			return rowView;
-		}
-
-		@Override
-		public int getCount() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public Object getItem(int position) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
 
 	}
+
 }
