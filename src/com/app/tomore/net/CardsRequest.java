224fa0bb3df33dceb99d301754be6ec6b40683d9
neobility <neo.wu2@gmail.com>
2014-12-27 23:21:37 -0500

    update member detail

--- src/com/app/tomore/net/CardsRequest.java
+++ src/com/app/tomore/net/CardsRequest.java
@@ -35,12 +35,12 @@ public class CardsRequest {
 	}
 
 	/*
-	 * update card by memberID
+	 * update card by cardID
 	 */
 	// http://54.213.167.5/updateCardInfo.php?cardID=1&cardTitle=title&cardBarcode=barcode&cardNumber=123456
 	// &cardDes=Des&frontViewImage=~/image/123.jpg&backViewImage=~/image/123.jpg
-	public String updateCardInfo(String cardID, String cardTitle,
-			String cardBarcode, String cardDes) throws IOException, TimeoutException {
+	public String updateCardInfo(String cardID, String cardTitle, String cardBarcode,
+			String cardDes) throws IOException, TimeoutException {
 		baseRequest = new BasicHttpClient(url);
 		baseRequest.setConnectionTimeout(2000);
 		ParameterMap params = baseRequest.newParams().add("cardID", cardID)
@@ -48,6 +48,23 @@ public class CardsRequest {
 				.add("cardDes", cardDes);
 		HttpResponse httpResponse = baseRequest.post("/updateCardInfo.php",
 				params);
+		return httpResponse.getBodyAsString();
+	}
+
+	/*
+	 * upload card by memberID
+	 */
+	// http://54.213.167.5/uploadCard.php?memberID=34&cardDes=des&cardBarcode=barcode&cardTitle=title&cardType=1
+	public String uploadCard(String cardTitle, String cardBarcode,
+			String cardDes, String cardType, String memberID)
+			throws IOException, TimeoutException {
+		baseRequest = new BasicHttpClient(url);
+		baseRequest.setConnectionTimeout(2000);
+		ParameterMap params = baseRequest.newParams()
+				.add("cardTitle", cardTitle).add("cardBarcode", cardBarcode)
+				.add("cardDes", cardDes).add("cardType", cardType)
+				.add("memberID", memberID);
+		HttpResponse httpResponse = baseRequest.post("/uploadCard.php", params);
 		return httpResponse.getBodyAsString();
 	}
 }
