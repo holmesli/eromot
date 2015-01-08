a71600665acbdc70a04fa243adfc34dae783510a
Darren Liu <s.darren.liu@gmail.com>
2015-01-08 01:05:21 -0500

    Gother parses of login and register

--- src/com/app/tomore/net/UserCenterParse.java
+++ src/com/app/tomore/net/UserCenterParse.java
@@ -1,6 +1,24 @@
 package com.app.tomore.net;
 
-public class UserCenterParse {
-	
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+public class UserCenterParse {
+	public String parseLoginResponse(String result){
+		Gson gson = new Gson();
+		JsonElement jelement = new JsonParser().parse(result);
+	    JsonObject  jobject = jelement.getAsJsonObject();
+	    String loninResult = jobject.get("result").toString();
+		return loninResult;		
+	}
+	
+	public String parseRegisterResponse(String result){
+		Gson gson = new Gson();
+		JsonElement jelement = new JsonParser().parse(result);
+	    JsonObject  jobject = jelement.getAsJsonObject();
+	    String registerResult = jobject.get("result").toString();
+	    return registerResult;
+	}
 }
