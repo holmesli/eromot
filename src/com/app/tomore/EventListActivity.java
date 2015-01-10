22fb3eaaf3b79e94945536a594258c7caa789f94
Alex.Liu <alexliubo@gmail.com>
2015-01-09 22:40:58 -0500

    add new template activities

---
+++ src/com/app/tomore/EventListActivity.java
@@ -0,0 +1,15 @@
+package com.app.tomore;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EventListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_event_list);
+	}
+}
---
+++ src/com/app/tomore/ThreadReplyActivity.java
@@ -0,0 +1,12 @@
+package com.app.tomore;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ThreadReplyActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_thread_reply);
+	}
+}
